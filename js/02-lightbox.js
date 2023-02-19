179ffb2e187c6d912a5edba396082d2b896402cb
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-19 09:58:22 +0100

    end task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createGallery = () => {
+	const markup = galleryItems
+		.map(
+			({ preview, original, description }) =>
+				`<a class="gallery__item" href= ${original}>
+                <img
+                    class="gallery__image"
+                    src=${preview}
+                    alt=${description}
+                />
+                </a>`,
+		)
+		.join("");
+	gallery.insertAdjacentHTML("beforeend", markup);
+};
+createGallery();
+let galleryLightBox = new SimpleLightbox(".gallery a", {
+	showCounter: false,
+	captions: true,
+	captionsData: "alt",
+	captionDelay: 250,
+});
