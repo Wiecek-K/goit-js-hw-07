faa11ca8798a1b75f5a9af21f63a6f406d0cc429
Wiecek-K <krystiandwiecek@gmail.com>
2023-02-19 07:33:05 +0100

    end task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
 
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const createGallery = () => {
+	const markup = galleryItems
+		.map(
+			({ preview, original, description }) =>
+				`<div class="gallery__item">
+    <a class="gallery__link" href= ${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+      />
+    </a>
+  </div>`,
+		)
+		.join("");
+	gallery.insertAdjacentHTML("beforeend", markup);
+};
+createGallery();
+
+const openImage = (e) => {
+	if (e.target.nodeName !== "IMG") {
+		console.log(e.target.nodeName);
+		return;
+	}
+	e.preventDefault();
+	console.log(e.target.dataset.source);
+	basicLightbox.create(`<img src=${e.target.dataset.source}>`).show();
+};
+gallery.addEventListener("click", openImage);
 console.log(galleryItems);
